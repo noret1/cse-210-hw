367f98fb74d7f516e7f0857f541e606229f61aa1
noret1 <nabukenyaphiona3@gmail.com>
2026-07-09 04:46:44 +0300

    edit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! look at my Execrise1.");
+        Console.WriteLine("Hello world! look at my Exercise1 project.");
+
+        Console.WriteLine("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ");
     }
+
 }
+
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("what is your grade percentage? ");
+        int percentage = int.Parse(Console.ReadLine());
+        string letter = "";
+
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Grade: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations on passing this course!");
+        }
+        else
+        {
+            Console.WriteLine("Try harder next!");
+        }
+        
     }
 }
